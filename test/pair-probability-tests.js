9afc688c706ad81efe296098c9646b218ac616ea
John Beaumier <jbeaumie@thoughtworks.com>
2017-02-24 10:42:49 -0800

    Refactor | using arrow functions

--- test/pair-probability-tests.js
+++ test/pair-probability-tests.js
@@ -1,8 +1,8 @@
 var assert = require('assert');
 var calc = require('../src/pair-probability');
 
-describe('pairProbability', function() {
-    it('should give zero when there are no cards and no more cards to draw', function() {
+describe('pairProbability', () => {
+    it('should give zero when there are no cards and no more cards to draw', () => {
         assert.equal(0, calc(0));
     });
 });
