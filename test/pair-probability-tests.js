d0f852e38b6db12e6344f1a10c4010b747d155a2
John Beaumier <jbeaumie@thoughtworks.com>
2017-02-24 10:53:02 -0800

    Red | Pairs, adding case for drawing two cards

--- test/pair-probability-tests.js
+++ test/pair-probability-tests.js
@@ -2,7 +2,11 @@ var assert = require('assert');
 var calc = require('../src/pair-probability');
 
 describe('pairProbability', () => {
-    it('should give zero when there are no cards and no more cards to draw', () => {
+    it('should give zero when there are no cards to draw', () => {
         assert.equal(0, calc(0));
     });
+
+    it('should give 1/13 when there are 2 cards to draw', () => {
+        assert.equal(1 / 13, calc(2));
+    })
 });
