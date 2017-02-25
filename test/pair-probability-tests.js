ce89d70ed301c1012969be749cc7b60e3aafe564
John Beaumier <jbeaumie@thoughtworks.com>
2017-02-24 23:45:15 -0800

    Red | also reordered some unit tests

--- test/pair-probability-tests.js
+++ test/pair-probability-tests.js
@@ -6,15 +6,19 @@ describe('pairProbability', () => {
         assert.equal(0, calc(0));
     });
 
-    it('should give 1/13 when there are 2 cards to draw', () => {
-        assert.equal(1 / 13, calc(2));
-    });
-
     it('should give 1 when there are 14 cards left to draw', () => {
         assert.equal(1, calc(14));
     });
 
+    it('should give 1/13 when there are 2 cards to draw', () => {
+        assert.equal(1 / 13, calc(2));
+    });
+
     it('should give 1/13 + 12/13*2/13 when there are 3 cards to draw', () => {
         assert.equal(1/13 + 12/13*2/13, calc(3));
+    });
+
+    it('should give 1/13 + 12/13*2/13 + 11/13*3/13 when there are 3 cards to draw', () => {
+        assert.equal(1/13 + 12/13*2/13 + 11/13*3/13, calc(3));
     });
 });
