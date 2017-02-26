9e2ff93e287f890b0acb358c75d5d0524e15db36
John Beaumier <jbeaumie@thoughtworks.com>
2017-02-26 09:30:56 -0800

    Green

--- src/pair-probability.js
+++ src/pair-probability.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const pairProbability = (cardsLeft) => {
     if (cardsLeft === 0) {
         return 0;
@@ -6,8 +8,11 @@ const pairProbability = (cardsLeft) => {
     }
 
     const pairOnEarlierCards = pairProbability(cardsLeft - 1);
-    const avoidEarlierPairs = (13 - (cardsLeft - 2)) / 13;
     const pairOnNthCard = (cardsLeft - 1) / 13;
+    let avoidEarlierPairs = 1;
+    for (let i = 1; i + 1 < cardsLeft; i++) {
+        avoidEarlierPairs = avoidEarlierPairs * (13 - (cardsLeft - 1 - i)) / 13;
+    }
     
     return pairOnEarlierCards + avoidEarlierPairs * pairOnNthCard; 
 };
