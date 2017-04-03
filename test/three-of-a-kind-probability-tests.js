5ab882dc03f2bafe5585c64e760dd866e93bbcbe
John Beaumier <jbeaumie@thoughtworks.com>
2017-04-02 21:26:10 -0700

    Red | Added test for first nonzero result

--- test/three-of-a-kind-probability-tests.js
+++ test/three-of-a-kind-probability-tests.js
@@ -5,4 +5,7 @@ describe('ThreeOfAKindProbability', () => {
     it('should give zero when there are no cards to draw', () => {
         assert.equal(0, calc(0));
     });
+    it('should give 1/13^2 when there are three cards to draw', () => {
+        assert.equal(Math.pow(1/13, 2), calc(3));
+    });
 });
