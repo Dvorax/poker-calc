9cb8d9208753ee5ea621ff7f7308b0f97bcf6dda
John Beaumier <jbeaumie@thoughtworks.com>
2017-04-03 07:48:02 -0700

    Green

--- src/three-of-a-kind-probability.js
+++ src/three-of-a-kind-probability.js
@@ -2,7 +2,7 @@
 var factorial = require('./factorial');
 
 const threeOfAKindProbability = (cardsLeft) => {
-    return 0;
+    return cardsLeft == 0 ? 0 : Math.pow(1 / 13, 2);
 };
 
 module.exports = threeOfAKindProbability;
